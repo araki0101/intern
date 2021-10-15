9867656fb93ecccb49a50980ca95a41622a5f0fe
荒木建太郎 <araki@arakikentarounoMacBook-ea.local>
2021-10-15 10:35:53 +0900

    commit SampleApp Test

---
+++ SampleApp/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root "sessions#login"
+  resources :sessions
+  get 'sessions/new'
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#userinfo'
+  delete '/logout', to: 'sessions#destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
